0f44e9001312039807631388fe2f5bf2a364a59f
Srivalli-2007 <sreevallinalli@gmail.com>
2026-06-02 09:03:36 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+function login(){
+
+const username =
+document.getElementById("username").value;
+
+const password =
+document.getElementById("password").value;
+
+if(username === "admin" &&
+password === "admin123"){
+
+localStorage.setItem("loggedIn","true");
+
+window.location.href="index.html";
+
+}else{
+
+alert("Invalid Credentials");
+
+}
+
+}
